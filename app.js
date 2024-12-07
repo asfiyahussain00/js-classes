4dbba8e37491d6b71f184d4f954010c11a5f0711
Asfiya hussain <126233785+asfiyahussain00@users.noreply.github.com>
2024-12-07 13:05:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,146 @@
+//  primitive data-types in JavaScript
+
+
+// primitive data-types in number
+
+// let num = 34;
+//  let num1 = 90.45;
+
+// console.log("value of num" ,  num );
+// console.log("value of num" ,  num1);
+
+// To check the data type 
+// console.log( typeof(num1));
+
+
+
+// primitive data-types in  string
+ 
+// let name = "asfi";
+// let name1 = "hafsa";
+
+// console.log("value of num" ,  name );
+// console.log("value of num" ,  name1);
+
+
+
+
+// primitive data-types in  undefined
+// let x ;
+// console.log(x);
+
+
+
+// primitive data-types in  null
+// let y  = null;
+// console.log(y);
+
+
+// primitive data-types in  Boolean
+
+// let isclick = (false)
+//  console.log(isclick)
+// // To check the data type 
+// console.log( typeof(isclick));
+
+
+
+// primitive data-types in  BigInit
+
+// let bigNum = 123422222222222222222222222222222222222n
+// console.log(bigNum)
+
+
+
+// primitive data-types in  symbols
+
+// let sym = Symbol("Hello");
+// console.log(sym);
+
+
+// Non-primitive Data Types
+
+// const student = {  
+//       stdName  : "Asfiya" ,
+//       FName  : "Hussain" ,
+//       RollNo : 2345 ,
+//       Coruse : "Web development"};
+
+// console.log(student);
+
+// //  To change any key value 
+//   student["stdName"] = "hafsa";
+//   console.log(student["stdName"]);
+
+
+
+
+//  This is use for singal line Comment
+/*  This is USE  for multiple line line Comment*/
+
+
+
+
+// use for new line
+// alert("happy coding!\n prevent this page from creating additional dialogs");
+
+//  in documte.write we use <br> for new line
+// document.write("Happy coding <br> hello javascript")
+
+// class 5 
+
+// Operaters In Js
+
+// let num1 = 2; 
+// let num2 = 10; 
+// let num3 = 6;
+// sum of Number
+// console.log("sum of number =" , num1+num2+num3);
+// sub of Number
+// console.log("sub of number =" , num1-num2-num3)
+// Multiply of Number
+// console.log("multiply of number =" , num1*num2*num3);
+// Divide of Number
+// console.log("divide of number =" , num1/+num2/num3);
+// Percentage of Number
+// console.log("percentage of number =" , num3%num1);
+// module of Number
+// console.log( "power of num1 and num2 =",num3**num1 );
+
+// increment & decrement
+// let a = 10 ;
+ // in mathmactic concept
+// a = a+2; 12
+ // a = a-2; 10
+ // console.log(a)
+
+// in javascript for add one we write
+// let x = 20;
+// x--;
+// x++
+// console.log(x);
+
+
+// let a = 20 ;
+// a++
+// console.log(a); // 21
+// console.log(++a);//22
+// console.log(a--);//22
+// console.log(a);//21
+
+
+// let x = 49 ;
+// console.log(x++); //49
+// console.log(--x); //49 
+
+
+
+//  let r = 12; 
+//  r = r++; 
+//  r--; 
+//  console.log(--r); //10
+
+
 //  LOOP
 // for( let a=0; a<=10; a++){
 //     document.write("hanadi", "<br/>")
